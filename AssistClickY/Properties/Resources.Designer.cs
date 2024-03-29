06484d2c0a2637e8440e5f447a706e6f274455cf
DiamondScythe <nhutoan399@gmail.com>
2024-03-29 11:19:14 +0700

    changed saving location for screenshots, added automatic naming for screenshots

--- AssistClickY/Properties/Resources.Designer.cs
+++ AssistClickY/Properties/Resources.Designer.cs
@@ -10,7 +10,8 @@
 
 namespace AssistClickY.Properties {
     using System;
-
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -18,19 +19,19 @@ namespace AssistClickY.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
-
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
@@ -44,7 +45,7 @@ namespace AssistClickY.Properties {
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
@@ -58,74 +59,75 @@ namespace AssistClickY.Properties {
                 resourceCulture = value;
             }
         }
-
+        
         /// <summary>
-        ///   Looks up a localized string similar to ts.ItemName.
+        ///   Looks up a localized string similar to AssistClickY.
         /// </summary>
         public static string AppDisplayName {
             get {
                 return ResourceManager.GetString("AppDisplayName", resourceCulture);
             }
         }
-
+        
         /// <summary>
-        ///   Looks up a localized string similar to ShellGoBackButton.
+        ///   Looks up a localized string similar to Home.
+        /// </summary>
+        public static string HomePageTitle {
+            get {
+                return ResourceManager.GetString("HomePageTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to HotkeySetup.
+        /// </summary>
+        public static string HotkeySetupPageTitle {
+            get {
+                return ResourceManager.GetString("HotkeySetupPageTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to AssistClickY\Screenshots.
+        /// </summary>
+        public static string ScreenshotSavePath {
+            get {
+                return ResourceManager.GetString("ScreenshotSavePath", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Go back.
         /// </summary>
         public static string ShellGoBackButton {
             get {
                 return ResourceManager.GetString("ShellGoBackButton", resourceCulture);
             }
         }
-
+        
         /// <summary>
-        ///   Looks up a localized string similar to ShellHamburgerButtonName.
+        ///   Looks up a localized string similar to Open or close navigation.
         /// </summary>
         public static string ShellHamburgerButtonName {
             get {
                 return ResourceManager.GetString("ShellHamburgerButtonName", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Home.
         /// </summary>
-        public static string HomePageTitle
-        {
-            get
-            {
-                return ResourceManager.GetString("HomePageTitle", resourceCulture);
-            }
-        }
-
-        /// <summary>
-        ///   Looks up a localized string similar to Home.
-        /// </summary>
-        public static string ShellHomePage
-        {
-            get
-            {
+        public static string ShellHomePage {
+            get {
                 return ResourceManager.GetString("ShellHomePage", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to HotkeySetup.
         /// </summary>
-        public static string HotkeySetupPageTitle
-        {
-            get
-            {
-                return ResourceManager.GetString("HotkeySetupPageTitle", resourceCulture);
-            }
-        }
-
-        /// <summary>
-        ///   Looks up a localized string similar to HotkeySetup.
-        /// </summary>
-        public static string ShellHotkeySetupPage
-        {
-            get
-            {
+        public static string ShellHotkeySetupPage {
+            get {
                 return ResourceManager.GetString("ShellHotkeySetupPage", resourceCulture);
             }
         }
--- AssistClickY/ViewModels/HomeViewModel.cs
+++ AssistClickY/ViewModels/HomeViewModel.cs
@@ -3,9 +3,6 @@ using AssistClickY.Enums;
 using AssistClickY.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using ScreenCapture.NET;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
 using System.IO;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -63,13 +60,26 @@ namespace AssistClickY.ViewModels
         [RelayCommand]
         private static void Screenshot()
         {
+            var saveFolderPath = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+                    Properties.Resources.ScreenshotSavePath);
+
+            // Check if the directory exists, if not, create it
+            if (!Directory.Exists(saveFolderPath))
+            {
+                Directory.CreateDirectory(saveFolderPath);
+            }
+
+            var fileName = $"screenshot_{DateTime.Now:yyyyMMddHHmmss}.jpg";
+            var fullPath = Path.Combine(saveFolderPath, fileName);
+
             using var bitmap = new Bitmap(1920, 1080);
             using (var g = Graphics.FromImage(bitmap))
             {
                 g.CopyFromScreen(0, 0, 0, 0,
                 bitmap.Size, CopyPixelOperation.SourceCopy);
             }
-            bitmap.Save("filename.jpg", ImageFormat.Jpeg);
+            bitmap.Save(fullPath, ImageFormat.Jpeg);
         }
     }
 }
