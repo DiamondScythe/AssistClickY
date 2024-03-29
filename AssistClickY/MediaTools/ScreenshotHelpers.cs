9c1d3adfc5dd2a9b8f11384eea8aa5800d18355d
DiamondScythe <nhutoan399@gmail.com>
2024-03-29 11:45:30 +0700

    made dedicated helper class for screenshots in the MediaTools folder (idk im bad at naming)

---
+++ AssistClickY/MediaTools/ScreenshotHelpers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssistClickY.MediaTools
+{
+    public static class ScreenshotHelpers
+    {
+        public static void TakeScreenshot()
+        {
+
+            var saveFolderPath = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+                    Properties.Resources.ScreenshotSavePath);
+
+            // Check if the directory exists, if not, create it
+            if (!Directory.Exists(saveFolderPath))
+            {
+                Directory.CreateDirectory(saveFolderPath);
+            }
+
+            var fileName = $"screenshot_{DateTime.Now:yyyyMMddHHmmss}.jpg";
+            var fullPath = Path.Combine(saveFolderPath, fileName);
+
+            using var bitmap = new Bitmap(1920, 1080);
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.CopyFromScreen(0, 0, 0, 0,
+                bitmap.Size, CopyPixelOperation.SourceCopy);
+            }
+            bitmap.Save(fullPath, ImageFormat.Jpeg);
+        }
+    }
+}
--- AssistClickY/ViewModels/HomeViewModel.cs
+++ AssistClickY/ViewModels/HomeViewModel.cs
@@ -6,6 +6,7 @@ using CommunityToolkit.Mvvm.Input;
 using System.IO;
 using System.Drawing;
 using System.Drawing.Imaging;
+using AssistClickY.MediaTools;
 
 namespace AssistClickY.ViewModels
 {
@@ -39,6 +40,7 @@ namespace AssistClickY.ViewModels
         {
             try
             {
+                // TODO: Add some validation
                 var hotkey = new Hotkey
                 {
                     HotkeyCombination = HotkeyCombination.ToString(),
@@ -58,28 +60,6 @@ namespace AssistClickY.ViewModels
             }
         }
         [RelayCommand]
-        private static void Screenshot()
-        {
-            var saveFolderPath = Path.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
-                    Properties.Resources.ScreenshotSavePath);
-
-            // Check if the directory exists, if not, create it
-            if (!Directory.Exists(saveFolderPath))
-            {
-                Directory.CreateDirectory(saveFolderPath);
-            }
-
-            var fileName = $"screenshot_{DateTime.Now:yyyyMMddHHmmss}.jpg";
-            var fullPath = Path.Combine(saveFolderPath, fileName);
-
-            using var bitmap = new Bitmap(1920, 1080);
-            using (var g = Graphics.FromImage(bitmap))
-            {
-                g.CopyFromScreen(0, 0, 0, 0,
-                bitmap.Size, CopyPixelOperation.SourceCopy);
-            }
-            bitmap.Save(fullPath, ImageFormat.Jpeg);
-        }
+        private static void Screenshot() => ScreenshotHelpers.TakeScreenshot();
     }
 }
